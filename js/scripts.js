c9fcaeb2118d46d2e825bf57f08fe8ff826144c4
mbiyu2017 <benedictmbiyuthangwa@gmail.com>
2018-05-14 09:46:27 +0300

    application adds and displays the new number generated after rolling the dice

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 //business logic
 var randomNumber= function(){
-
   return Math.floor(Math.random()*6)+1;
 }
 
+function Playerscore(currentscore, newscore,realscore){
+  this.currentscore = currentscore;
+  this.newscore = newscore;
+  this.realscore = realscore;
+}
 
+var firstPlayer = new Playerscore(0,0,0);
+var secondPlayer = new Playerscore(0,0,0);
 
-
-
-
+Playerscore.prototype.totalscore = function(){
+  this.newscore+=this.currentscore
+}
 
 
 
@@ -19,9 +25,27 @@ var randomNumber= function(){
 
 //UI logic
 $(document).ready(function(){
-  $(".roll-dice").click(function(){
-    var random = randomNumber();
-    $(".score").text(random);
-    $(".player-score").text(random);
+
+
+  $(".roll-dice1").click(function(){
+  firstPlayer.currentscore =  randomNumber();
+  firstPlayer.newscore+=firstPlayer.currentscore;
+    $(".score1").text(firstPlayer.currentscore);
+    $(".currscore1").text(firstPlayer.newscore);
+})
+
+  $(".hold-score1").click(function(){
+    $(".score1").text();
   });
+
+
+
+    $(".roll-dice2").click(function(){
+    secondPlayer.currentscore =  randomNumber();
+
+      $(".score2").text(secondPlayer.currentscore);
+      $(".player-score2").text();
+
+
+    });
 });

96532e630f1cbc77f9241bd47efaf5527115c550
mbiyu2017 <benedictmbiyuthangwa@gmail.com>
2018-05-15 08:08:09 +0300

    add styling to application using css and bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -34,9 +34,11 @@ $(document).ready(function(){
     $(".currscore1").text(firstPlayer.newscore);
 
     if(firstPlayer.currentscore == 1){
-      $(".roll-dice1").hide();
-      $(".roll-dice2").show();
-      alert("Its player 2's turn to roll the dice!");
+      $(".roll-dice1").prop("disabled",true);
+      $(".hold-score1").prop("disabled",true);
+      $(".roll-dice2").prop("disabled",false);
+      $(".hold-score2").prop("disabled",false);
+      alert("OOPS!Your dice rolled on number 1.Its player 2's turn to roll the dice!");
     }
 
 })
@@ -46,11 +48,13 @@ $(document).ready(function(){
     $(".player-score1").text(firstPlayer.realscore);
     firstPlayer.currentscore=0;
     firstPlayer.newscore=0;
-    $(".roll-dice1").hide();
-    $(".roll-dice2").show();
+    $(".roll-dice1").prop("disabled",true);
+    $(".hold-score1").prop("disabled",true);
+    $(".roll-dice2").prop("disabled",false);
+    $(".hold-score2").prop("disabled",false);
     alert("Player 2's turn")
     if (firstPlayer.realscore >= 100){
-      alert("YEEE! Player 1 has won the game!")
+      alert("YEEE! Player 1 has Won the Game!")
     }
   });
 
@@ -63,9 +67,11 @@ $(document).ready(function(){
       $(".currscore2").text(secondPlayer.newscore);
 
       if(secondPlayer.currentscore == 1){
-        $(".roll-dice2").hide();
-        $(".roll-dice1").show();
-        alert("Its player 1's turn to roll the dice!");
+        $(".roll-dice2").prop("disabled",true);
+        $(".hold-score2").prop("disabled",true);
+        $(".roll-dice1").prop("disabled",false);
+        $(".hold-score1").prop("disabled",false);
+        alert("OOPS!Your dice rolled to number 1.Its player 1's turn to roll the dice!");
       }
   })
 
@@ -74,16 +80,14 @@ $(document).ready(function(){
       $(".player-score2").text(secondPlayer.realscore);
       secondPlayer.currentscore=0;
       secondPlayer.newscore=0;
-      $(".roll-dice2").hide();
-      $(".roll-dice1").show();
-      alert("Player 1's turn")
+      $(".roll-dice2").prop("disabled",true);
+      $(".hold-score2").prop("disabled", true);
+      $(".roll-dice1").prop("disabled",false);
+      $(".hold-score1").prop("disabled",false);
+      alert("Player 1's turn");
       if(secondPlayer.realscore >=100 ){
-        alert("YEEE! Player 2 Won the Game")
+        alert("YEEEY! Player 2 has Won the Game")
       }
     });
 
-
-
-
-
-    });
+});
